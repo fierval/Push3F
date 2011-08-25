45db0591dceb474a60b02efbe5aafa7616104416
Boris <fierval@gmail.com>
2011-08-24 21:13:33 -0700

    Adding StockTypes class - holds all the type stacks.

--- Interpreter/TypeFactory.fs
+++ Interpreter/TypeFactory.fs
@@ -9,37 +9,47 @@ module TypeFactory =
     open Type
     open push.stack.Stack
 
-    // Class that wraps a PushTypeFactory. The core of all things
-    // All push types should inherit from this class
-    type PushTypeFactory () =
 
-        static member internal CreatePushObject (t:System.Type) : #PushTypeBase * string =
-            let ci = t.GetConstructor(Array.empty)
-            let name = (t.GetCustomAttributes(typeof<PushTypeAttribute>, false).[0] :?> PushTypeAttribute).Name
-            let pushObj = ci.Invoke(Array.empty)
-            let typedObj = 
-                match pushObj with
-                | :? #PushTypeBase as pushO -> pushO
-                | _ -> failwithf "Can't happen, but the type is not push-based"
-            typedObj, name
+    let createPushObject (t:System.Type) : #PushTypeBase * string =
+        let ci = t.GetConstructor(Array.empty)
+        let name = (t.GetCustomAttributes(typeof<PushTypeAttribute>, false).[0] :?> PushTypeAttribute).Name
+        let pushObj = ci.Invoke(Array.empty)
+        let typedObj = 
+            match pushObj with
+            | :? #PushTypeBase as pushO -> pushO
+            | _ -> failwithf "Can't happen, but the type is not push-based"
+        typedObj, name
 
-        // discovers the types and maps them by name
-        static member internal DiscoverByAssemblyAttribute(attribute : System.Type, assembly : string option) =
-            assembly 
-            |> loadTypes
-            |> getAnnotatedTypes attribute
-            |> Seq.fold (fun map t -> 
-                            let pushObj, name = PushTypeFactory.CreatePushObject t
-                            map |> Map.add name pushObj) Map.empty
+    // discovers the types and maps them by name
+    let discoverByAssemblyAttribute (attribute : System.Type) (assembly : string option) =
+        assembly 
+        |> loadTypes
+        |> getAnnotatedTypes attribute
+        |> Seq.fold (fun map t -> 
+                        let pushObj, name = createPushObject t
+                        map |> Map.add name pushObj) Map.empty
 
 
-        //static function to run through the assemblies and discover new types
-        static member internal DiscoverPushTypes(?assembly : string) =
-            PushTypeFactory.DiscoverByAssemblyAttribute(typeof<PushTypeAttribute>, assembly)
+    //static function to run through the assemblies and discover new types
+    let discoverPushTypes (assembly : string option) =
+        discoverByAssemblyAttribute typeof<PushTypeAttribute> assembly
 
-        static member internal CreatePushStacks map =
-            stacksInit map
-                          
+    // stack of currently implemented types
+    let typeStacks (map : Map<string, #PushTypeBase>) : Map<string, Stack<#PushTypeBase>> = map |> Map.map (fun key o -> empty)
 
+    let appendMaps map1 map2 = ((map1 |> Map.toList) @ (map2 |> Map.toList)) |> Map.ofList
+
+    // keeps the actual stock types
+    type StockTypes(? assembly : string) = 
+        let mutable ptypes = discoverPushTypes assembly
+        let mutable stacks = typeStacks ptypes
+
+        member t.Stacks with get() = stacks
+        member t.Types with get() = ptypes
+
+        member t.appendStacksFromAssembly (assembly : string) =
+            let newTypes = discoverPushTypes (Some assembly)
+            ptypes <- appendMaps ptypes newTypes
+            stacks <- typeStacks ptypes
 
 

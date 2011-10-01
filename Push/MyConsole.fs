2508b2d8d90c9bcc855c04fe4d9b17c666964cc1
Boris <fierval@gmail.com>
2011-10-01 16:16:45 -0700

    First drop of command line client.

---
+++ Push/MyConsole.fs
@@ -0,0 +1,21 @@
+﻿namespace Push
+
+open System
+open System.Collections.Generic
+open Arguments
+
+module MyConsole =
+        
+    let Run args =
+
+        // Define what arguments are expected
+        let defs = [
+            {ArgInfo.Command="a1"; Description="Argument 1"; Required=true };
+            {ArgInfo.Command="a2"; Description="Argument 2"; Required=false } ]
+
+        // Parse Arguments into a Dictionary
+        let parsedArgs = Arguments.ParseArgs args defs
+        
+        // TODO add your code here 
+        Arguments.DisplayArgs parsedArgs
+        Console.ReadLine() |> ignore
---
+++ Push/Program.fs
@@ -0,0 +1,13 @@
+﻿namespace Push
+
+open System
+
+module Program =   
+
+    [<EntryPoint>]
+    let Main(args) = 
+
+        MyConsole.Run args
+
+        // main entry point return
+        0

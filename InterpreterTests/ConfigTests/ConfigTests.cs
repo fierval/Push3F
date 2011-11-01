fa291b3334aae6fc2d37a40f2c07bf29aa943cb7
Boris <fierval@gmail.com>
2011-10-31 23:04:14 -0700

    Configuration reader for Genetics.

---
+++ InterpreterTests/ConfigTests/ConfigTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using push.config;
+using System.Collections.Generic;
+using System;
+
+namespace ConfigTests
+{
+    [TestClass]
+    public class ConfigTests
+    {
+        [TestMethod]
+        [DeploymentItem("sampleConfig.xml")]
+        public void ConfigReaderTest()
+        {
+            ConfigReader reader = new ConfigReader("sampleConfig.xml");
+
+            dynamic config = reader.Read();
+
+            Assert.AreEqual<int>(100, config.popSize);
+            Assert.AreEqual<int>(300, config.maxCodePoints);
+            Assert.AreEqual<int>(100, config.numGenerations);
+            Assert.AreEqual<float>((float).7, config.probMutation);
+            Assert.AreEqual<float>((float).3, config.probCrossover);
+            Assert.AreEqual("CODE.NOOP", config.getArgument);
+            Assert.AreEqual("(INTEGER.- FLOAT.FROMINTEGER)", config.getResult);
+            Assert.AreEqual("2", reader.GetSampleValue(0, "In").ToString());
+            Assert.AreEqual("2", reader.GetSampleValue(0, "Out").ToString());
+        }
+
+    }
+}
---
+++ Push.Config/ConfigReader.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using System.Xml.Linq;
+using System.IO;
+using System.Dynamic;
+
+namespace push.config
+{
+    public class ConfigReader
+    {
+        XElement root;
+        dynamic config;
+
+        public ConfigReader(string fileName)
+        {
+            if (string.IsNullOrWhiteSpace(fileName))
+            {
+                throw new ArgumentNullException(fileName);
+            }
+
+            if (!File.Exists(fileName))
+            {
+                throw new IOException("File does not exist: " + fileName);
+            }
+
+            this.root = XDocument.Load(fileName).Root;
+            this.config = new ExpandoObject();
+        }
+
+        public dynamic Read()
+        {
+            config.popSize = GetInt("PopulationSize");
+            config.maxCodePoints = GetInt("MaxCodePoints");
+            config.numGenerations = GetInt("NumGenerations");
+            config.probCrossover = GetFloat("ProbCrossover");
+            config.probMutation = GetFloat("ProbMutation");
+            config.getArgument = GetString("GetArgument");
+            config.getResult = GetString("GetResult");
+            config.samples = GetSampleCollection();
+
+            return config;
+        }
+
+        int GetInt(string name)
+        {
+            return int.Parse((from p in root.Descendants(name) select p).First().Value.Trim());
+        }
+
+        float GetFloat(string name)
+        {
+            return float.Parse((from p in root.Descendants(name) select p).First().Value.Trim());
+        }
+
+        string GetString(string name)
+        {
+            return (from p in root.Descendants(name) select p).First().Value.Trim();
+        }
+
+        List<dynamic> GetSampleCollection()
+        {
+            var samples = from s in this.root.Descendants("Sample") select s;
+            var collection = new List<dynamic>();
+            foreach (var s in samples)
+            {
+                dynamic sample = new ExpandoObject();
+                foreach (var child in s.Descendants())
+                {
+                    (sample as IDictionary<String, object>)[child.Name.ToString()] = child.Value.Trim();
+                }
+                collection.Add(sample);
+            }
+            return collection;
+        }
+
+        public object GetSampleValue(int index, string val)
+        {
+            IDictionary<String, object> dict = config.samples[index];
+            return dict[val];
+        }
+
+    }
+}
